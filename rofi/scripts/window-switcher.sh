24137682c4c3dfcd6b5d423082906f0cb4b3146d
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-03-13 12:16:28 +0900

    fix(rofi): zen browserのアイコンが表示されていなかった不具合を修正。

--- rofi/scripts/window-switcher.sh
+++ rofi/scripts/window-switcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-selected=$(hyprctl clients -j | jq -r '.[] | select(.workspace.id != 1) | select(.title != "ピクチャーインピクチャー") | "\(.title) (\(.address))\u0000icon\u001f\(.class)"' | rofi -dmenu -show-icons -theme ~/.config/rofi/themes/main.rasi)
+selected=$(hyprctl clients -j | jq -r '.[] | select(.workspace.id != 1) | select(.title != "ピクチャーインピクチャー") | "\(.title) (\(.address))\u0000icon\u001f\(if .class == "zen" then "zen-browser" else .class end)"' | rofi -dmenu -show-icons -theme ~/.config/rofi/themes/main.rasi)
 
 address=$(echo "$selected" | grep -oP '\(0x[0-9a-f]+\)' | tr -d '()')
 if [ -n "$address" ]; then
