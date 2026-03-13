aa369cff3a98a41aeb1a707392838ef2f430f0bf
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-03-13 20:56:24 +0900

    feat(hypr,deploy): Spotifyを全画面にして戻すと、位置がずれていたのを、完璧にもとの位置に戻す機能を追加。

--- deploy.sh
+++ deploy.sh
@@ -35,6 +35,7 @@ ln -snf "$DOTFILES_DIR/hypr/scripts/clock-kitty.conf" "$HOME/.config/hypr/script
 ln -snf "$DOTFILES_DIR/hypr/scripts/clock.py" "$HOME/.config/hypr/scripts/clock.py"
 ln -snf "$DOTFILES_DIR/hypr/scripts/monitor-watch.sh" "$HOME/.config/hypr/scripts/monitor-watch.sh"
 ln -snf "$DOTFILES_DIR/hypr/scripts/photo-widget.sh" "$HOME/.config/hypr/scripts/monitor-watch.sh"
+ln -snf "$DOTFILES_DIR/hypr/scripts/spotify-watch.sh" "$HOME/.config/hypr/scripts/spotify-watch.sh"
 
 mkdir -p "$HOME/.config/eww/scripts"
 ln -snf "$DOTFILES_DIR/eww/eww.scss" "$HOME/.config/eww/eww.scss"
