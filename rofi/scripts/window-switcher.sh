b693a08bd4067e76a5743ff06c077a84e09d52bd
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-03-13 12:11:02 +0900

    fix(rofi,hypr): rofiの、マウスが行くのにフォーカスが元のウィンドウのままなのと、メニューでウィンドウ選択後にOKというのが出る不具合を修正。-dmenuモードへ切り替えることで解決。

--- rofi/scripts/window-switcher.sh
+++ rofi/scripts/window-switcher.sh
@@ -1,2 +1,9 @@
-#!/bin/bash 
-hyprctl clients -j | jq -r '.[] | select(.workspace.id != 1) | .title'
+#!/bin/bash
+selected=$(hyprctl clients -j | jq -r '.[] | select(.workspace.id != 1) | select(.title != "ピクチャーインピクチャー") | "\(.title) (\(.address))\u0000icon\u001f\(.class)"' | rofi -dmenu -show-icons -theme ~/.config/rofi/themes/main.rasi)
+
+address=$(echo "$selected" | grep -oP '\(0x[0-9a-f]+\)' | tr -d '()')
+if [ -n "$address" ]; then
+    coords=$(hyprctl clients -j | jq -r ".[] | select(.address == \"$address\") | \"\(.at[0]+(.size[0]/2|floor)) \(.at[1]+(.size[1]/2|floor))\"")
+    hyprctl dispatch focuswindow "address:$address"
+    hyprctl dispatch movecursor $coords
+fi
